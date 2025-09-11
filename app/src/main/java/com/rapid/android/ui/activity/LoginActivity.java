dcf77230e2e08b59bf7c329881e8d78c51d003e3
yuan <yuan0x00@qq.com>
2025-09-11 21:43:09 +0800

    update

--- app/src/main/java/com/rapid/android/ui/activity/LoginActivity.java
+++ app/src/main/java/com/rapid/android/ui/activity/LoginActivity.java
@@ -2,7 +2,11 @@ package com.rapid.android.ui.activity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.core.base.ui.BaseActivity;
@@ -11,6 +15,8 @@ import com.rapid.android.databinding.ActivityLoginBinding;
 import com.rapid.android.viewmodel.LoginViewModel;
 
 public class LoginActivity extends BaseActivity<LoginViewModel, ActivityLoginBinding> {
+
+    private Toolbar toolbar;
 
     @Override
     protected LoginViewModel createViewModel() {
@@ -58,6 +64,35 @@ public class LoginActivity extends BaseActivity<LoginViewModel, ActivityLoginBin
                 ToastUtils.showLongToast(msg);
             }
         });
+    }
+
+    @Override
+    protected void initializeViews() {
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        toolbar = findViewById(com.webview.R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setTitle("登录或注册");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
